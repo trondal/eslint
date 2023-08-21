f4582d6773938c7ed72bd999a6d774db9b26ebee
Trond Albinussen <trond.albinussen@blinq.no>
2023-08-21 12:02:44 +0200

    Added unicorn

--- strict.js
+++ strict.js
@@ -7,6 +7,7 @@ module.exports = {
     "airbnb/hooks",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:unicorn/recommended",
     "prettier", // disabled conflicting rules with prettier, Prettier must be installed in host project
   ],
   overrides: [
@@ -25,6 +26,8 @@ module.exports = {
       "error",
       { groups: ["builtin", "external", "internal", "parent", "sibling"] },
     ],
+    "import/no-default-export": "error",
+    "import/prefer-default-export": "off",
     "no-console": ["error", { allow: ["warn", "error", "info"] }],
     "no-nested-ternary": "off",
     // Allow most functions to rely on type inference. If the function is exported, then `@typescript-eslint/explicit-module-boundary-types` will ensure it's typed.
@@ -37,6 +40,12 @@ module.exports = {
     // This must be off due to @typescript-eslint/no-use-before-define
     "no-use-before-define": "off",
     "react/react-in-jsx-scope": "off",
+    "unicorn/filename-case": [
+      "error",
+      {
+        cases: { camelCase: true, pascalCase: true },
+      },
+    ],
   },
   parserOptions: {
     ...base.parserOptions,
